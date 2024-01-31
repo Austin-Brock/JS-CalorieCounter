4a42028a25e3221bfd09bf0710a0d5db22406541
AustinBrockUGA <135121147+Austin-Brock@users.noreply.github.com>
2024-01-30 22:20:36 -0500

    updated the addEntry function

--- script.js
+++ script.js
@@ -40,4 +40,5 @@ function isInvalidInput(str) {
 
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll();
 }
