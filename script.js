3b467664150661c315f44b76ac4063c31c910ad9
AustinBrockUGA <135121147+Austin-Brock@users.noreply.github.com>
2024-02-09 14:30:10 -0500

    updated calculateCalories function

--- script.js
+++ script.js
@@ -82,4 +82,5 @@ function calculateCalories(e) {
   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 }
