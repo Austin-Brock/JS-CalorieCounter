7d4d97a5fd3537a1a6c5445c2f56e54514b62f26
AustinBrockUGA <135121147+Austin-Brock@users.noreply.github.com>
2024-02-09 14:50:20 -0500

    updated function with if for error

--- script.js
+++ script.js
@@ -83,4 +83,7 @@ function calculateCalories(e) {
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
   const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+  if(isError) {
+    return;
+  }
 }
